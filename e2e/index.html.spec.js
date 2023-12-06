703415db077d7df582b953e94f18d298fa2e04fe
Lukas712 <lukascarvalho712@gmail.com>
2023-12-05 23:42:56 -0300

    Vitest com muitos problemas, não verifica nada

--- e2e/index.html.spec.js
+++ e2e/index.html.spec.js
@@ -5,7 +5,7 @@ test.describe("index.html", () => {
   test("deve ter o charset em UTF-8 no meta dentro do head", async ({
     page,
   }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     await expect(page.locator("meta[charset]")).toHaveAttribute(
       "charset",
       "UTF-8"
@@ -15,7 +15,7 @@ test.describe("index.html", () => {
   test('deve ter um meta viewport com atributo content com "width=device-width, initial-scale=1.0" no head', async ({
     page,
   }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     await expect(page.locator('meta[name="viewport"]')).toHaveAttribute(
       "content",
       "width=device-width, initial-scale=1.0"
@@ -23,31 +23,31 @@ test.describe("index.html", () => {
   });
 
   test("deve ter o lang em pt no html", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     await expect(page.locator("html")).toHaveAttribute("lang", "pt");
   });
 
   test("deve ter um título no head com o número da atividade e nome do aluno", async ({
     page,
   }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     await expect(page).toHaveTitle(/DCC202 - Atividade 11: (.*)/);
   });
 
   test("deve ter um main com algum conteúdo", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     await expect(page.locator("main")).toBeVisible();
   });
 
   test("deve ter um título no main com o nome do aluno", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     await expect(page.locator("main > h1")).toHaveText(
       /DCC202 - Atividade 11: (.*)/
     );
   });
 
   test("o main deve ter um parágrafo logo após o título", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     const p = page.locator("main > h1+p");
     await expect(p).toBeVisible();
     await expect(p).toHaveText(
@@ -56,14 +56,14 @@ test.describe("index.html", () => {
   });
 
   test("Deve ter um script módulo na cabeça do documento para main.js.", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     const script = page.locator("head > script");
     await expect(script).toHaveAttribute('src', 'main.js');
     await expect(script).toHaveAttribute('type', 'module');
   });
 
   test("deve ter um parágrafo sem uma seção com id entrada e conteúdo editável", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     const p = await page.locator("main > section> p");
 
     await expect(p).toBeEditable();
@@ -75,7 +75,7 @@ test.describe("index.html", () => {
   });
 
   test("deve ter dois botões com ids e conteúdos definidos", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     const b1 = await page.locator("main > section> #adicionar");
     await expect(b1).toHaveText(
       /Adicionar/
@@ -90,19 +90,19 @@ test.describe("index.html", () => {
   });
 
   test("deve ter uma lista ordenada com id 'itens'", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     const ol = await page.locator("main > section> ol");
     await expect(ol).toHaveId('itens');
   });
 
   test("a lista deve ter três itens inicialmente", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     const itensDaLista = await page.locator("#itens > li");
     await expect(itensDaLista).toHaveCount(3);
   });
 
   test("Um novo item deve ser adicionado à lista ao digitar e clicar em adicionar", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     const p = await page.locator("main > section> p");
     const b1 = await page.locator("main > section> #adicionar");
     await p.selectText();
@@ -116,7 +116,7 @@ test.describe("index.html", () => {
   });
 
   test("A lista deve ficar vazia ao clicar em 'Limpar Lista'", async ({ page }) => {
-    await page.goto(`http://localhost:5501`);
+    await page.goto(`http://localhost:5500`);
     const p = await page.locator("main > section> p");
     const b1 = await page.locator("main > section> #limpar");
     const itensDaLista = await page.locator("#itens > li");
